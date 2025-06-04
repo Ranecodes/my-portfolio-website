b4b26efb621441852ddf26d39834670637e94515
Ranehobasi Egbe <ranehobasi@gmail.com>
2025-06-04 15:59:59 +0100

    WIP functionalities left

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-var loader = document.getElementById('preloader');
+var loader = document.getElementById("preloader");
 
-window.addEventListener('load', function () {
-    loader.style.display = 'none';
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+
+  // dots animation logic
+  const dotsElements = document.querySelectorAll(".dots");
+
+  dotsElements.forEach((el) => {
+    let count = 0;
+    setInterval(() => {
+      el.textContent = ".".repeat(count);
+      count = (count + 1) % 4; // cycles through 0, 1, 2, 3
+    }, 500);
+  });
 });
-
-
-
